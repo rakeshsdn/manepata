b04aa273cb0acc22b40ca6b6570a7f80bc0beef7
rakesh <rakeshsdnr@gmail.com>
2024-08-24 20:33:53 +0530

    Update attendace controller

--- src/main/java/in/manepata/security/usermanager/controllers/AttendanceController.java
+++ src/main/java/in/manepata/security/usermanager/controllers/AttendanceController.java
@@ -1,22 +1,25 @@
 package in.manepata.security.usermanager.controllers;
 
+import in.manepata.security.usermanager.dto.AttendanceDto;
+import in.manepata.security.usermanager.dto.TakeAttendance;
 import in.manepata.security.usermanager.entities.Attendance;
 import in.manepata.security.usermanager.services.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/attendance")
+@RequestMapping("/api/attendance")
 public class AttendanceController {
 
     @Autowired
     private AttendanceService attendanceService;
 
     @PostMapping
-    public Attendance takeAttendance(@RequestBody Attendance attendance) {
-        return attendanceService.takeAttendance(attendance);
+    public List<AttendanceDto> takeAttendance(@RequestBody List<AttendanceDto> attendanceDtos) {
+        return attendanceService.takeAttendance(attendanceDtos);
     }
 
     @GetMapping("/student/{studentId}")
@@ -33,5 +36,12 @@ public class AttendanceController {
     public List<Attendance> getAttendanceByCenterAndStudent(@PathVariable Long centerId, @PathVariable Long studentId) {
         return attendanceService.getAttendanceByCenterAndStudent(centerId, studentId);
     }
+
+    @GetMapping("/list/center/{centerId}")
+    public List<TakeAttendance> getAttendanceListByCenter(@PathVariable Long centerId){
+
+        return attendanceService.getAttendanceListByCenter(centerId);
+    }
+
 
 }
