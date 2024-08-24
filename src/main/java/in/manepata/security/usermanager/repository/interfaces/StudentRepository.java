2bb9c9649e2b70419d362b42d1b708ef367e5c92
rakesh <rakeshsdnr@gmail.com>
2024-08-24 20:32:45 +0530

    added findByCenterId db call

--- src/main/java/in/manepata/security/usermanager/repository/interfaces/StudentRepository.java
+++ src/main/java/in/manepata/security/usermanager/repository/interfaces/StudentRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import in.manepata.security.usermanager.entities.Student;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByCenterId(Long centerId);
 }
